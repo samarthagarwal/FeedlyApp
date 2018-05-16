945e31536071f28f76d24bc0da63487d4614a0e9
Samarth Agarwal <samarthagarwal@live.com>
2018-05-17 00:09:36 +0530

    Designing of Login page Done

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-login',
+  templateUrl: 'login.html'
 })
-export class HomePage {
+export class LoginPage {
 
   constructor(public navCtrl: NavController) {
 

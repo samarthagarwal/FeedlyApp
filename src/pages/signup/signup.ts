dcfaeb91970ca2ce92c94d8b0f5418df3b30d827
Samarth Agarwal <samarthagarwal@live.com>
2018-05-19 17:12:04 +0530

    Signup done with firebase auth

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import firebase from 'firebase';
 
 @Component({
   selector: 'page-signup',
@@ -8,7 +8,32 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
+  name: string = "";
+  email: string = "";
+  password: string = "";
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  signup(){
+    firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+    .then((data) => {
+      
+      console.log(data)
+
+      let newUser: firebase.User = data.user;
+      newUser.updateProfile({
+        displayName: this.name,
+        photoURL: ""
+      }).then(() => {
+        console.log("Profile Updated")
+      }).catch((err) => {
+        console.log(err)
+      })
+
+    }).catch((err) => {
+      console.log(err)
+    })
   }
 
   goBack(){

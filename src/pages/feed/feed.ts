67fe238088ccf02e0aa7a5416515e57fcfabb9b0
Samarth Agarwal <samarthagarwal@live.com>
2018-06-19 23:41:18 +0530

    Get Token and Save it

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -6,6 +6,7 @@ import { LoginPage } from '../login/login';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 import { HttpClient } from '@angular/common/http';
 import { CommentsPage } from '../comments/comments';
+import { Firebase } from '@ionic-native/firebase'
 
 @Component({
   selector: 'page-feed',
@@ -20,10 +21,36 @@ export class FeedPage {
   infiniteEvent: any;
   image: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private loadingCtrl: LoadingController, private toastCtrl: ToastController, private camera: Camera, private http: HttpClient, private actionSheetCtrl: ActionSheetController, private alertCtrl: AlertController, private modalCtrl: ModalController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private loadingCtrl: LoadingController, private toastCtrl: ToastController, private camera: Camera, private http: HttpClient, private actionSheetCtrl: ActionSheetController, private alertCtrl: AlertController, private modalCtrl: ModalController, private firebaseCordova: Firebase) {
     this.getPosts();
+
+    this.firebaseCordova.getToken().then((token) => {
+      console.log(token)
+
+      this.updateToken(token, firebase.auth().currentUser.uid);
+
+    }).catch((err) => {
+      console.log(err)
+    })
+
+
   }
 
+
+  updateToken(token: string, uid: string){
+
+    firebase.firestore().collection("users").doc(uid).set({
+      token: token,
+      tokenUpdate: firebase.firestore.FieldValue.serverTimestamp()
+    }, {
+      merge: true
+    }).then(() => {
+      console.log("token saved to cloud firestore");
+    }).catch(err => {
+      console.log(err);
+    })
+
+  }
 
   getPosts() {
 
